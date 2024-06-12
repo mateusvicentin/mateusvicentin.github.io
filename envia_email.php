518ac52da28fbae7c9da401ab4809f90642135ef
Mateus Vicentin <31457038+mateusvicentin@users.noreply.github.com>
2024-06-12 14:06:42 -0300

    portfolio email

---
+++ envia_email.php
@@ -0,0 +1,22 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+
+    $to = "mateusvicentin8@gmail.com"; 
+    $headers = "From: $name <$email>";
+    $message_body = "Assunto: $subject\n\n$message";
+
+    // Envia o email
+    if (mail($to, $subject, $message_body, $headers)) {
+        echo "Obrigado por entrar em contato, $name. Sua mensagem foi enviada com sucesso!";
+    } else {
+        echo "Desculpe, ocorreu um erro ao enviar sua mensagem. Por favor, tente novamente mais tarde.";
+    }
+} else {
+    header("Location: /index.html"); 
+    exit();
+}
+?>
